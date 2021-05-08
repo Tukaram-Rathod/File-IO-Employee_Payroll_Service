57f17acb54bc1846fadd9d29a5d6367fb6d6a5f6
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-05-08 15:40:29 +0530

    Added.used File IO read the File

--- src/main/java/employeepayrollservice/EmployeePayrollService.java
+++ src/main/java/employeepayrollservice/EmployeePayrollService.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 
 public class EmployeePayrollService {
 
+
     public enum IOService{CONSOLE_IO,FILE_IO,DB_IO,REST_IO}
     private List<EmployeePayrollData> employeePayrollDataList;
 
@@ -21,20 +22,25 @@ public class EmployeePayrollService {
     public static void main(String[] args) {
         ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<EmployeePayrollData>();
         EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
-        Scanner consoleInputReader = new Scanner(System.in);
-        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.readEmployeePayrollData(IOService.FILE_IO);
         employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
     }
-    //method to read data
-    private void readEmployeePayrollData(Scanner consoleInputReader) {
-        System.out.println("Enter Employee Id:");
-        int id=consoleInputReader.nextInt();
-        System.out.println("Enter Employee name:");
-        consoleInputReader.nextLine();
-        String name=consoleInputReader.nextLine();
-        System.out.println("Enter Employee salary:");
-        double salary=consoleInputReader.nextInt();
-        employeePayrollDataList.add(new EmployeePayrollData(id,name,salary));
+    public long readEmployeePayrollData(IOService ioService) {
+        if(ioService.equals(IOService.CONSOLE_IO)) {
+            Scanner consoleInputReader = new Scanner(System.in);
+            System.out.println("Enter Employee Id:");
+            int id=consoleInputReader.nextInt();
+            System.out.println("Enter Employee name:");
+            consoleInputReader.nextLine();
+            String name=consoleInputReader.nextLine();
+            System.out.println("Enter Employee salary:");
+            double salary=consoleInputReader.nextInt();
+            employeePayrollDataList.add(new EmployeePayrollData(id,name,salary));
+        }
+        List<String> employeeList = null;
+        if(ioService.equals(IOService.FILE_IO))
+            employeeList = new EmployeePayrollFileIOService().readData();
+        return employeeList.size();
     }
     //method to write data on console
     public void writeEmployeePayrollData(IOService ioService) {
@@ -43,7 +49,6 @@ public class EmployeePayrollService {
         else if (ioService.equals(IOService.FILE_IO))
             new EmployeePayrollFileIOService().writeData(employeePayrollDataList);
     }
-    //method to count entries in a file
     public long countEntries() {
         long entries = 0;
         try {
@@ -53,6 +58,8 @@ public class EmployeePayrollService {
         }
         return entries;
     }
+
+
     //method to count entries in a file
     public long count(IOService ioService) {
         if(ioService.equals(IOService.FILE_IO))
